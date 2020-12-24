0d20d9de089b65e44dd17d88ebc0d94069be946b
zxuan-app <1171102256@student.mmu.edu.my>
2020-12-24 14:57:45 +0800

    Button added

--- app/src/main/java/com/example/lab5/MainActivity.java
+++ app/src/main/java/com/example/lab5/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.lab5;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView img;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        img = findViewById(R.id.img1);
+
+        img.setBackgroundResource(R.drawable.pic1);
+
+
     }
 }
