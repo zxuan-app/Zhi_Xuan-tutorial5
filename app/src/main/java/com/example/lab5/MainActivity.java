7fc408cbc98770a0604f8555c5779c191d887f9d
zxuan-app <1171102256@student.mmu.edu.my>
2020-12-24 15:11:54 +0800

    Implemented changed pictures

--- app/src/main/java/com/example/lab5/MainActivity.java
+++ app/src/main/java/com/example/lab5/MainActivity.java
@@ -3,20 +3,36 @@ package com.example.lab5;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     ImageView img;
+    Button btn;
+    private int number;
+    int[] image = {R.drawable.pic1, R.drawable.pic2, R.drawable.pic3, R.drawable.pic4};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         img = findViewById(R.id.img1);
+        btn = findViewById(R.id.btn1);
 
-        img.setBackgroundResource(R.drawable.pic1);
+        img.setBackgroundResource(image[0]);
 
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                number ++;
+                number = number%image.length;
+                img.setBackgroundResource(image[number]);
+            }
+        });
 
     }
 }
